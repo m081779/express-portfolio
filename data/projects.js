fac5a5b211c9ed1dc21e766c24859ccd340fbdcb
m081779 <marcoprincipio@hotmail.com>
2018-01-28 00:28:01 -0500

    added error image, broke projects out into a json object to iterate over

---
+++ data/projects.js
@@ -0,0 +1,62 @@
+  [
+  {
+    "title": "Mongo NYT Scraper",
+    "url": "https://mongo-scraper-principio.herokuapp.com/",
+    "img": "/images/scraper300x200.jpg"
+  },
+  {
+    "title": "React NYT Article Search",
+    "url": "http://nyt-react-map.herokuapp.com/",
+    "img": "/images/nytreact300x200.jpg"
+  },
+  {
+    "title": "Watcha-Da-Movie",
+    "url": "https://principio-movie-list.herokuapp.com/",
+    "img": "/images/movie300x200.jpg"
+  },
+  {
+    "title": "Sweet Talk Dating",
+    "url": "https://map-sweet-talk.herokuapp.com/",
+    "img": "/images/sweet300x200.jpg"
+  },
+  {
+    "title": "Poke-Map Challenge",
+    "url": "https://m081779.github.io/PokeRPG/",
+    "img": "/images/poke300x200.jpg"
+  },
+  {
+    "title": "Trivia Game",
+    "url": "https://m081779.github.io/TriviaGame/",
+    "img": "/images/trivia300x200.jpg"
+  },
+  {
+    "title": "Supervillain Hangman",
+    "url": "https://m081779.github.io/Hangman-Game/",
+    "img": "/images/supervillain300x200.jpg"
+  },
+  {
+    "title": "BUGZ: Fight or Die!",
+    "url": "https://m081779.github.io/week-4-game/",
+    "img": "/images/bugz300x200.jpg"
+  },
+  {
+    "title": "HTML5 Canvas ball animation",
+    "url": "https://m081779.github.io/bouncing-balls/",
+    "img": "/images/balls300x200.jpg"
+  },
+  {
+    "title": "HTML5 Canvas Draw",
+    "url": "https://m081779.github.io/Canvas-rainbow-draw/",
+    "img": "/images/draw300x200.jpg"
+  },
+  {
+    "title": "Node CLI Hangman",
+    "url": "https://github.com/m081779/Node-Hangman",
+    "img": "/images/nodehangman300x200.jpg"
+  },
+  {
+    "title": "Train Scheduler",
+    "url": "https://m081779.github.io/Train-Scheduler/",
+    "img": "/images/train300x200.jpg"
+  }
+]
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const fs = require('fs');
 
 router.get('/', function(req, res) {
-	console.log('firing')
-	res.render('index', { title: 'Marcoprincipio.com' });
+	let projects = JSON.parse(fs.readFileSync('./data/projects.js', 'utf8'))
+	res.render('index', { title: 'MarcoPrincipio.com', projects: projects });
 });
 
 

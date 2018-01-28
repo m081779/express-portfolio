6d5a668ad950e31806fc33cae3ca6ef6250486cc
m081779 <marcoprincipio@hotmail.com>
2018-01-27 20:47:07 -0500

    added link to nyt react app, moved some of the javascript around

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {		
+$(document).ready(function () {
 	$("a").click(function (event) {
 
 		if (this.hash!=="") {
 			event.preventDefault();
 			var hash = this.hash;
-			
+
 			$('html').animate({
 				scrollTop: $(hash).offset().top
 
@@ -15,53 +15,102 @@ $(document).ready(function () {
 
 	});
 
+
+	function checkInput() {
+		let counter = 0
+		let args = Array.prototype.slice.call(arguments);
+		args.forEach(item => {
+			let placeholder = $(item).attr('placeholder');
+			if ($(item).val().trim()==='') {
+				$(item).attr('placeholder', 'Pretty please enter!');
+				setTimeout(()=>{$(item).attr('placeholder', placeholder);}, 1000*2);
+			} else {
+				counter++
+				$(item).attr('placeholder', placeholder);
+			}
+		});
+		let bool =  counter===4 ? true : false;
+		return bool;
+	}
+
+	function clearInputs() {
+		let args = Array.prototype.slice.call(arguments);
+		args.forEach(item => $(item).val(''));
+	}
+
+	$('#submit').on('click', function () {
+		event.preventDefault();
+		let message = {};
+		let valid = checkInput('#name','#email', '#number', '#text');
+		console.log(valid)
+		if(valid) {
+			message.name = $('#name').val().trim();
+			message.email = $('#email').val().trim();
+			message.number = $('#number').val().trim();
+			message.text = $('#text').val().trim();
+			clearInputs('#name','#email', '#number', '#text');
+			console.log(message)
+			$.ajax("/email", {
+				type: 'POST',
+				data: message
+			}).done(function(data) {
+				let firstName = message.name.split(' ');
+				firstName = firstName[0].charAt(0).toUpperCase() + firstName[0].slice(1);;
+				let str = `Your message was sent successfully, ${firstName}. Thanks for contacting me, I will be in touch shortly!`
+						$('#infoText').text(str)
+						$('#cookie').addClass('animateCookie');
+					});
+		}
+	});
+
+
+
+	var letterArr = [];
+
+	document.onkeyup = function (event) {
+		var konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+		letterArr.push(event.keyCode);
+		if (letterArr.length>11) {
+			letterArr.shift();
+		}
+		if (konami.join('')===letterArr.join('')) {
+			document.getElementById('main').setAttribute('src', '../images/contra.jpg')
+			var snd  = new Audio();
+			var src  = document.createElement("source");
+			src.type = "audio/mpeg";
+			src.src  = "../audio/Contra.mp3";
+			snd.appendChild(src);
+			snd.play();
+			$('.contraModal').addClass('contraModalShow');
+		}
+	}
+
+	$('#close').on('click', function () {
+		$('.contraModal').removeClass('contraModalShow');
+	})
+
+	$('body').on('click','.cubeRotate', function () {
+
+		var value = $(this).text();
+		if (value==='PORTFOLIO'){
+			$('#cube').css({
+				'transform': 'rotateY(-90deg) scale(0.9)',
+				// 'padding-top': '2.5%'
+			});
+		}
+		else if (value==='CONTACT') {
+			$('#cube').css({
+				'transform': 'rotateY(90deg) scale(0.9)',
+				// 'padding-top': '2.5%'
+			});
+		}
+
+		else if (value==='ABOUT') {
+			$('#cube').css({
+				'transform': 'rotateY(0deg) scale(0.9)',
+				// 'padding-top': '2.5%'
+			});
+		}
+	});
+
 });
-
-var letterArr = [];
-
-document.onkeyup = function (event) {
-	var konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
-	letterArr.push(event.keyCode);
-	if (letterArr.length>11) {
-		letterArr.shift();
-	}
-	if (konami.join('')===letterArr.join('')) {
-		document.getElementById('main').setAttribute('src', '../images/contra.jpg')
-		var snd  = new Audio();
-		var src  = document.createElement("source");
-		src.type = "audio/mpeg";
-		src.src  = "../audio/Contra.mp3";
-		snd.appendChild(src);
-		snd.play();
-		$('.contraModal').addClass('contraModalShow');
-	}
-}
-
-$('#close').on('click', function () {
-	$('.contraModal').removeClass('contraModalShow');
-})
-
-$('body').on('click','.cubeRotate', function () {
-
-	var value = $(this).text();
-	if (value==='PORTFOLIO'){
-		$('#cube').css({
-			'transform': 'rotateY(-90deg) scale(0.9)',
-			// 'padding-top': '2.5%'
-		});
-	}
-	else if (value==='CONTACT') {
-		$('#cube').css({
-			'transform': 'rotateY(90deg) scale(0.9)',
-			// 'padding-top': '2.5%'
-		});
-	}
-
-	else if (value==='ABOUT') {
-		$('#cube').css({
-			'transform': 'rotateY(0deg) scale(0.9)',
-			// 'padding-top': '2.5%'
-		});
-	}
-});
-
